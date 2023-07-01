96e96c75486f1acc91d2bebae5202e2b06953f2f
Nour Krimesh <nour.krimesh@gmail.com>
2023-07-02 02:49:50 +0300

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+
+const employee = {
+    name: 'Nour',
+    streetAddress: 'wxyz 34'
+};
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const emp = {...employee};
+    emp[key] = value;
+
+    return emp;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) {
+    const emp = {...employee};
+    delete emp[key];
+
+    return emp;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+
+    return employee;
+}
